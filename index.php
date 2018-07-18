63986784365c1c3827b356688511dad52ea539a1
Tomek Świerszcz <tomek.swierszcz@gmail.com>
2018-07-18 13:58:52 +0200

    html -> php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    ini_set ('display_errors', 'on');
+    ini_set ('log_errors', 'on');
+    ini_set ('display_startup_errors', 'on');
+    ini_set ('error_reporting', E_ALL);
+    echo "\nadmin at ".__FILE__;
+?>
+
+
+<html>
+           <title>USOS24.pl - Admin panel</title>
+       </html>
